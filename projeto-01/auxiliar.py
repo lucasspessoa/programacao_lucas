c62c139c117309f22c04d23fca3cd347411006d6
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2024-03-06 16:26:52 -0300

    etapa-06.03

--- projeto-01/auxiliar.py
+++ projeto-01/auxiliar.py
@@ -6,3 +6,6 @@ currentMouseX, currentMouseY = pyautogui.position()
 
 print(currentMouseX)
 print(currentMouseY)
+
+# time.sleep(5)
+# pyautogui.click(x=762, y=293, clicks=180)
--- projeto-01/principal.py
+++ projeto-01/principal.py
@@ -1,14 +1,30 @@
 # 287*2 = 574 clicks necessários para SUBs
 import pyautogui as py
 import time as t
+import datetime
 
 link_ole = "https://ola.oleconsignado.com.br"
+data_atual = datetime.date.today()
+data_anterior = data_atual - datetime.timedelta(days=30)
 
 py.press('win')
+t.sleep(1)
 py.write('chrome')
 py.press('enter')
 t.sleep(1)
-py.write(link_ole, interval=0.1)
+py.write(link_ole, interval=0.01)
 py.press('enter')
 t.sleep(2)
-py.click(x=698, y=440, clicks=2)
+py.press('tab')
+py.write('LIDIANGFT38620')
+py.press('tab')
+py.write('Gftcredmais@27')
+py.press('tab')
+py.press('enter')
+t.sleep(8)
+py.click(x=1119, y=336)
+py.click(x=1003, y=132)
+py.click(x=297, y=219)
+py.click(x=347, y=261)
+py.click(x=182, y=597)
+py.click(x=347, y=261)
