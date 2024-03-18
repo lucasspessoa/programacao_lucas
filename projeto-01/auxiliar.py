bea52312e864b1c229441b369dc42f36f0dcd4d4
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2024-03-18 10:50:49 -0300

    etapa-18.03

--- projeto-01/auxiliar.py
+++ projeto-01/auxiliar.py
@@ -1,11 +1,11 @@
 import pyautogui
 import time
 
-time.sleep(5)
-currentMouseX, currentMouseY = pyautogui.position()
+# time.sleep(5)
+# currentMouseX, currentMouseY = pyautogui.position()
 
-print(currentMouseX)
-print(currentMouseY)
+# print(currentMouseX)
+# print(currentMouseY)
 
 # time.sleep(5)
-# pyautogui.click(x=762, y=293, clicks=180)
+# pyautogui.click(clicks=250)
